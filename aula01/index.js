21e51e4570058427a19944e94e264b74866557e2
danieltvaz <danieltostes@live.com>
2022-08-22 23:31:21 -0300

    feat: lesson 01

--- aula01/index.js
+++ aula01/index.js
@@ -1,4 +1,34 @@
 "use strict";
-const titleElement = document.getElementById("title");
-if (titleElement)
-    titleElement.innerText = "Olá mundo";
+const resultado = document.getElementById("resultado");
+const numero1 = document.getElementById("numero1");
+const numero2 = document.getElementById("numero2");
+const calcularBotao = document.getElementById("calcular");
+const containerResultado = document.getElementById("container-resultado");
+const botoesOperadores = Array.from(document.querySelectorAll(".operador-button"));
+botoesOperadores.forEach((botao) => {
+    botao.addEventListener("click", () => renderizarResultado(botao.dataset.value));
+});
+function calcular(operador) {
+    const valor1 = parseInt(numero1.value);
+    const valor2 = parseInt(numero2.value);
+    if (valor1 && valor2) {
+        switch (operador) {
+            case "+":
+                return (valor1 + valor2).toFixed(2).toString();
+            case "-":
+                return (valor1 - valor2).toFixed(2).toString();
+            case "*":
+                return (valor1 * valor2).toFixed(2).toString();
+            case "/":
+                return (valor1 / valor2).toFixed(2).toString();
+            default:
+                return "";
+        }
+    }
+    return "";
+}
+function renderizarResultado(operador) {
+    const resultValue = document.createElement("p");
+    resultValue.innerText = calcular(operador).trim();
+    containerResultado === null || containerResultado === void 0 ? void 0 : containerResultado.appendChild(resultValue);
+}
--- aula01/index.ts
+++ aula01/index.ts
@@ -1,3 +1,38 @@
-const titleElement = document.getElementById("title");
+const resultado = document.getElementById("resultado");
+const numero1: HTMLElement | null = document.getElementById("numero1");
+const numero2: HTMLElement | null = document.getElementById("numero2");
+const calcularBotao = document.getElementById("calcular");
+const containerResultado = document.getElementById("container-resultado");
 
-if (titleElement) titleElement.innerText = "Olá mundo";
+const botoesOperadores = Array.from(document.querySelectorAll(".operador-button")) as HTMLButtonElement[];
+
+botoesOperadores.forEach((botao) => {
+  botao.addEventListener("click", () => renderizarResultado(botao.dataset.value));
+});
+
+function calcular(operador: "+" | "-" | "*" | "/" | string | undefined) {
+  const valor1 = parseInt((numero1 as HTMLInputElement).value);
+  const valor2 = parseInt((numero2 as HTMLInputElement).value);
+  if (valor1 && valor2) {
+    switch (operador) {
+      case "+":
+        return (valor1 + valor2).toFixed(2).toString();
+      case "-":
+        return (valor1 - valor2).toFixed(2).toString();
+      case "*":
+        return (valor1 * valor2).toFixed(2).toString();
+      case "/":
+        return (valor1 / valor2).toFixed(2).toString();
+      default:
+        return "";
+    }
+  }
+  return "";
+}
+
+function renderizarResultado(operador: "+" | "-" | "*" | "/" | string | undefined) {
+  const resultValue = document.createElement("p");
+
+  resultValue.innerText = calcular(operador).trim();
+  containerResultado?.appendChild(resultValue);
+}
