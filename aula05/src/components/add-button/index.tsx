03bb6a869f21e369cd4ba51a7d8a9b2879a0dd4c
danieltvaz <danieltostes@live.com>
2022-09-10 12:17:11 -0300

    feat: button props

--- aula05/src/components/add-button/index.tsx
+++ aula05/src/components/add-button/index.tsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 
+import { ButtonHTMLAttributes } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 
-export default function AddButton() {
+export default function AddButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className="addbutton__button">
+    <button className="addbutton__button" {...props}>
       <FontAwesomeIcon icon={faSquarePlus} color="#fff" />
       Adicionar
     </button>
