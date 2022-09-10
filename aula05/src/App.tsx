bd848edd544c59e182ce9524fcaf858805972897
danieltvaz <danieltostes@live.com>
2022-09-10 19:40:43 -0300

    refactor: fn reference problem

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -4,17 +4,22 @@ import ListItem from "./components/molecules/list-item";
 import MainContainer from "./components/layout/main-container";
 import SearchBar from "./components/molecules/search-bar";
 import TodoList from "./components/molecules/todo-list";
+import localStorageHandler from "./utils/local-storage-handler";
+import { useEffect } from "react";
 import useList from "./hooks/useList";
 
 export default function App() {
   const { items, addItem, removeItem, updateItem } = useList();
+  const { onBootDataHandler } = localStorageHandler();
+
+  useEffect(() => onBootDataHandler(), []);
 
   return (
     <MainContainer>
       <SearchBar addItem={addItem} />
       <TodoList>
         {items.map((item) => (
-          <ListItem item={item} key={item.id} removeItem={() => removeItem(item)} updateItem={() => updateItem(item)} />
+          <ListItem item={item} key={item.id} removeItem={removeItem} updateItem={updateItem} />
         ))}
       </TodoList>
     </MainContainer>
