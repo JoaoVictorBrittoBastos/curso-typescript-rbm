978e03a589f455945d6bc564afabefc2bb8349ce
danieltvaz <danieltostes@live.com>
2022-09-10 16:49:30 -0300

    feat: add components to app

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,12 +1,22 @@
-import AddButton from "./components/add-button";
-import React from "react";
+import AddButton from "./components/atoms/add-button";
+import { Item } from "./types/item";
+import ListItem from "./components/molecules/list-item";
+import MainContainer from "./components/layout/main-container";
+import SearchBar from "./components/molecules/search-bar";
+import TodoList from "./components/molecules/todo-list";
+import useList from "./hooks/useList";
 
-function App() {
+export default function App() {
+  const { items, addItem, removeItem, updateItem } = useList();
+
   return (
-    <div className="App">
-      <AddButton />
-    </div>
+    <MainContainer>
+      <SearchBar addItem={addItem} />
+      <TodoList>
+        {items.map((item) => (
+          <ListItem item={item} key={item.id} removeItem={() => removeItem(item)} updateItem={() => updateItem(item)} />
+        ))}
+      </TodoList>
+    </MainContainer>
   );
 }
-
-export default App;
